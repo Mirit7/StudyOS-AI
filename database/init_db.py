a5bfc7c8ce0a944807f02f369da995dea52df6c7
ved ghoghari <vedghoghari19@gmail.com>
2026-07-06 22:34:43 +0530

    nothing

--- database/init_db.py
+++ database/init_db.py
@@ -29,3 +29,6 @@ conn.commit()
 conn.close()
 
 print("Database Ready.")
+
+#This is preloaded database to show that accuracy &
+#by using app will automatically update the database with new data. As Shown in Graph has already preloaded data it will update as usage!
